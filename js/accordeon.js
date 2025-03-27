480a53ce046078fb42e9cd4337f3eb88f4eecef8
Anatolii Orlov <anatoly.orlov93@gmail.com>
2025-03-27 04:47:46 +0400

    some addons

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,22 +1,26 @@
 'use strict'
 
-const contents = document.querySelectorAll('.program-line__content');
-contents.forEach((elem) => {
-    const title = elem.querySelector('.program-line__title');
-    const descr = elem.querySelector('.program-line__descr');
+const accordeon = () => {
+    const contents = document.querySelectorAll('.program-line__content');
+    contents.forEach((elem) => {
+        const title = elem.querySelector('.program-line__title');
+        const descr = elem.querySelector('.program-line__descr');
 
-    title.addEventListener('click', () => {
-        const isActive = descr.classList.contains('active');
-        // descr.classList.toggle('active')
+        title.addEventListener('click', () => {
+            const isActive = descr.classList.contains('active');
+            // descr.classList.toggle('active')
 
-        contents.forEach(otherElem => {
-            const otherDescr = otherElem.querySelector('.program-line__descr');
-            otherDescr.classList.remove('active');
+            contents.forEach(otherElem => {
+                const otherDescr = otherElem.querySelector('.program-line__descr');
+                otherDescr.classList.remove('active');
+            });
+
+            if (!isActive) {
+                descr.classList.add('active');
+            }
         });
 
-        if (!isActive) {
-            descr.classList.add('active');
-        }
     });
+};
 
-});
+accordeon();
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const modalBtn = document.querySelector('.modal__button');
-const modal = document.querySelector('.modal');
-const modalInner = document.querySelector('.modal__inner');
-const courseBtn = document.querySelector('.course__button');
+const modal = () => {
+    const modalBtn = document.querySelector('.modal__button');
+    const modal = document.querySelector('.modal');
+    const modalInner = document.querySelector('.modal__inner');
+    const courseBtn = document.querySelector('.course__button');
 
-//Создаём "крестик" для закрытия модального окна
-const closeBtn = document.createElement('div');
-closeBtn.innerHTML = '&times;';
-closeBtn.style.cssText = `
+    //Создаём "крестик" для закрытия модального окна
+    const closeBtn = document.createElement('div');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.style.cssText = `
 position: absolute; 
 top: 15px; 
 right: 15px; 
@@ -20,39 +21,41 @@ z-index: 100;
 line-height: 1;
 `;
 
-closeBtn.addEventListener('mouseenter', () => {
-    closeBtn.style.color = '#000';
-});
+    closeBtn.addEventListener('mouseenter', () => {
+        closeBtn.style.color = '#000';
+    });
 
-closeBtn.addEventListener('mouseleave', () => {
-    closeBtn.style.color = '#888';
-});
+    closeBtn.addEventListener('mouseleave', () => {
+        closeBtn.style.color = '#888';
+    });
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-    //Добавляем "крестик" в модальное окно
-    modalInner.style.position = 'relative'
-    modalInner.append(closeBtn);
-});
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        //Добавляем "крестик" в модальное окно
+        modalInner.style.position = 'relative'
+        modalInner.append(closeBtn);
+    });
 
-courseBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-    //Добавляем "крестик" в модальное окно
-    modalInner.style.position = 'relative'
-    modalInner.append(closeBtn);
-});
+    courseBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        //Добавляем "крестик" в модальное окно
+        modalInner.style.position = 'relative'
+        modalInner.append(closeBtn);
+    });
 
-closeBtn.addEventListener('click', () => {
-    modal.style.display = '';
-    //Сбрасываем позиционирование при закрытии модального окна
-    modalInner.style.position = ''
-});
-
-modal.addEventListener('click', (event) => {
-    const modalContent = event.target.closest('.modal__inner');
-    if (!modalContent) {
+    closeBtn.addEventListener('click', () => {
         modal.style.display = '';
         //Сбрасываем позиционирование при закрытии модального окна
         modalInner.style.position = ''
-    }
-});
+    });
+
+    modal.addEventListener('click', (event) => {
+        const modalContent = event.target.closest('.modal__inner');
+        if (!modalContent) {
+            modal.style.display = '';
+            //Сбрасываем позиционирование при закрытии модального окна
+            modalInner.style.position = ''
+        }
+    });
+};
+modal();
